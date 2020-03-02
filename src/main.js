d4f440863e640570869d89418751b20152b848c1
M. Hogeveen <mhogeveen94@gmail.com>
2020-03-02 18:48:31 +0100

    Add navbar & sass file architecture

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+
 import router from './router'
 import store from './store'
+
 import vuetify from './plugins/vuetify'
+
+import './assets/sass/index.sass'
 
 Vue.config.productionTip = false
 
