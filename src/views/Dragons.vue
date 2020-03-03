55b255e9871ea8544578299393b2590a8e7515cc
M. Hogeveen <mhogeveen94@gmail.com>
2020-03-03 19:59:30 +0100

    Add active status tracker

--- src/views/Dragons.vue
+++ src/views/Dragons.vue
@@ -42,7 +42,23 @@
                   </v-card>
                 </v-col>
                 <v-col cols="9">
-                  <h1 class="header_title">{{ dragon.name | capitalize }}</h1>
+                  <v-row>
+                    <v-col cols="4" class="flex-row header_title">
+                      <h4 class="opacity05">name:</h4>
+                      <h1 class="">{{ dragon.name }}</h1>
+                    </v-col>
+                    <v-col cols="4" class="flex-row header_title">
+                      <h4 class="opacity05">id:</h4>
+                      <h1 class="">{{ dragon.id }}</h1>
+                    </v-col>
+                    <v-col cols="4" class="flex-row header_title">
+                      <h4 class="opacity05">active:</h4>
+                      <div
+                        :class="activeYN(dragon.active)"
+                      >
+                      </div>
+                    </v-col>
+                  </v-row>
                   <v-divider></v-divider>
                   <p class="header_description">{{ dragon.description }}</p>
                 </v-col>
@@ -69,6 +85,17 @@ export default {
   computed: {
     ...mapState(['dragonsAPI'])
   },
+  methods: {
+    activeYN (value) {
+      const activeY = 'activeY'
+      const activeN = 'activeN'
+      if (value) {
+        return activeY
+      } else {
+        return activeN
+      }
+    }
+  },
   created () {
     this.$store.dispatch('loadDragons')
   }
@@ -92,7 +119,32 @@ export default {
   .header
     &_title
       line-height: 1
-      padding: 10px 0 20px 0
+      padding: 10px 0 20px 1rem
+      h1
+        padding-left: 5px
     &_description
       padding: 20px 0 0 0
+
+  .flex-row
+    display: flex
+    flex-direction: row
+    align-items: center
+
+  .opacity05
+    opacity: 0.4
+
+  .activeY,
+  .activeN
+    height: 30px
+    width: 30px
+    border-radius: 50%
+    margin-left: 5px
+
+  .activeY
+    background: #328C6B
+    box-shadow: 0px 0px 5px 0px rgba(50,140,107,1)
+
+  .activeN
+    background: #CC333F
+    box-shadow: 0px 0px 5px 0px rgba(204,51,63,1)
 </style>
